959d17c81c658c360da748d11e9ff3287016ae8f
Interchained <president@worldvaporexpo.com>
2022-07-17 20:58:48 -0400

    aliens

---
+++ src/entities/token.ts
@@ -0,0 +1,114 @@
+import { Currency } from './currency'
+import invariant from 'tiny-invariant'
+import { getNetwork } from '@ethersproject/networks'
+import { getDefaultProvider } from '@ethersproject/providers'
+import { Contract } from '@ethersproject/contracts'
+
+import { ChainId } from '../constants'
+import ERC20 from '../abis/ERC20.json'
+import { validateAndParseAddress } from '../utils'
+
+let CACHE: { [chainId: number]: { [address: string]: number } } = {
+  [ChainId.MAINNET]: {
+    '0xE0B7927c4aF23765Cb51314A0E0521A9645F0E2A': 9 // DGD
+  }
+}
+
+/**
+ * Represents an ERC20 token with a unique address and some metadata.
+ */
+export class Token extends Currency {
+  public readonly chainId: ChainId
+  public readonly address: string
+
+  static async fetchData(
+    chainId: ChainId,
+    address: string,
+    provider = getDefaultProvider(getNetwork(chainId)),
+    symbol?: string,
+    name?: string
+  ): Promise<Token> {
+    const parsedDecimals =
+      typeof CACHE?.[chainId]?.[address] === 'number'
+        ? CACHE[chainId][address]
+        : await new Contract(address, ERC20, provider).decimals().then((decimals: number): number => {
+            CACHE = {
+              ...CACHE,
+              [chainId]: {
+                ...CACHE?.[chainId],
+                [address]: decimals
+              }
+            }
+            return decimals
+          })
+    return new Token(chainId, address, parsedDecimals, symbol, name)
+  }
+
+  constructor(chainId: ChainId, address: string, decimals: number, symbol?: string, name?: string) {
+    super(decimals, symbol, name)
+    this.chainId = chainId
+    this.address = validateAndParseAddress(address)
+  }
+
+  equals(other: Token): boolean {
+    // short circuit on reference equality
+    if (this === other) {
+      return true
+    }
+    const equivalent = this.chainId === other.chainId && this.address === other.address
+    if (equivalent) {
+      // reference the same token, must have the same decimals/symbol/name
+      invariant(this.decimals === other.decimals, 'DECIMALS')
+      if (this.symbol && other.symbol) invariant(this.symbol === other.symbol, 'SYMBOL')
+      if (this.name && other.name) invariant(this.name === other.name, 'NAME')
+    }
+    return equivalent
+  }
+
+  sortsBefore(other: Token): boolean {
+    invariant(this.chainId === other.chainId, 'CHAIN_IDS')
+    invariant(this.address !== other.address, 'ADDRESSES')
+    return this.address.toLowerCase() < other.address.toLowerCase()
+  }
+}
+
+/**
+ * Compares two currencies for equality
+ */
+export function currencyEquals(currencyA: Currency, currencyB: Currency): boolean {
+  if (currencyA instanceof Token && currencyB instanceof Token) {
+    return currencyA.equals(currencyB)
+  } else if (currencyA instanceof Token) {
+    return false
+  } else if (currencyB instanceof Token) {
+    return false
+  } else {
+    return currencyA === currencyB
+  }
+}
+
+export const WETH = {
+  [ChainId.MAINNET]: new Token(
+    ChainId.MAINNET,
+    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.ROPSTEN]: new Token(
+    ChainId.ROPSTEN,
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.RINKEBY]: new Token(
+    ChainId.RINKEBY,
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether')
+}
